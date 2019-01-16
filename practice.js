7e3e585877858b3edd125585f771ec3da2f35448
Mark Romero <markromero@Marks-MBP.ad.uvu.edu>
2019-01-15 17:10:05 -0700

    completed main porblems, extra problems left

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr) {
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr) {
+  return arr[arr.length - 1];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family) {
+  family.forEach(element => {
+    alert(element);
+  });
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters) {
+  for(let i = letters.length -1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +90,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums) {
+  let tempArry = [];
+  nums.forEach(number => {
+    if(number % 2 === 0) {
+      tempArry.push(number);
+    }
+  });
+  return tempArry;
+}
 
 
 
@@ -106,8 +126,19 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
+function divider(numbersArray) {
+  let evenArray = [];
+  let oddArray = [];
+  let resultArray = [];
 
+  numbersArray.forEach(number => {
+    number % 2 === 0 ? evenArray.push(number) : oddArray.push(number);
+  });
 
+  resultArray.push(evenArray);
+  resultArray.push(oddArray);
+  return resultArray;
+}
 
 ////////// PROBLEM 7 //////////
 
@@ -127,7 +158,10 @@ var getRandomArbitrary = function() {
 */
 
 //Code Here
-
+function finder(arr) {
+  let number = getRandomArbitrary();
+  return arr.includes(number);
+}
 
 
 ////////// PROBLEM 8 //////////
@@ -196,7 +230,13 @@ function addItem(myGroceryList, item) {
 */
 
 //Code Here
-
+function maker() {
+  let tempArray = [];
+  for(let i = 1; i <= 215; i++) {
+    tempArray.push(i);
+  }
+  return tempArray;
+}
 
 
 ////////// PROBLEM 10 //////////
